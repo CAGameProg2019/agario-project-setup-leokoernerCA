df1cec130a38d7c22a903a9b931184abc585887b
leokoernerCA <leo.koerner@concordacademy.org>
2019-04-04 09:14:30 -0400

    Some changes

--- main.js
+++ main.js
@@ -1,12 +1,33 @@
 let canvas = document.getElementById('main');
 let c = canvas.getContext('2d');
 
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+// let pos = new Vector(60,60);
+// let vel = new Vector(.5,.2);
+
+let pos = new Vector(5, 7);
+let vel = new Vector(1, 1);
+
+
 function init() {
-    alert("hello world!");
+    vel.scale(2);
+    vel.print(); // <2, 2>
+    pos.subVector(vel);
+    pos.print();  // <3, 5>
     update();
 }
 
 function update() {
+    c.clearRect(0, 0, canvas.width, canvas.height)
+    pos.addVector(vel);
+
+    c.beginPath();
+    c.arc(pos.x, pos.y, 50, 0, Math.PI*2, false)
+    c.closePath();
+    c.stroke();
+
 
     requestAnimationFrame(update);
 }
--- vector.js
+++ vector.js
@@ -1,7 +1,36 @@
 class Vector {
-    
-    constructor() {
 
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
     }
+
+    addVector(vec) {
+        this.x += vec.x;
+        this.y += vec.y;
+        return this;
+    }
+
+    toString() {
+        return '<'+this.x+', '+this.y+'>';
+    }
+
+    print() {
+        console.log(this.toString());
+    }
+
+    //functions to write for homework
+    subVector(vec) {
+        this.x -= vec.x;
+        this.y -= vec.y;
+        return this;
+    }
+
+    scale(s) {
+        this.x *= s;
+        this.y *= s;
+        return this;
+    }
+
 
 }

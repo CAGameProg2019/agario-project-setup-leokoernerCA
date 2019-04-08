4c8fa99e8991ab34d48d0ca25f7f5e33098516c2
leokoernerCA <leo.koerner@concordacademy.org>
2019-04-08 11:30:20 -0400

    in class changes april 8

--- main.js
+++ main.js
@@ -2,8 +2,11 @@ let canvas = document.getElementById('main');
 let c = canvas.getContext('2d');
 
 canvas.width = innerWidth;
-canvas.height = innerHeight;
+canvas.height = innerHeight-10;
 
+let mpos;
+
+let player;
 let foods = [];
 
 let colors = [
@@ -20,6 +23,11 @@ function randomColor() {
 }
 
 function init() {
+
+    mpos =  new Vector(canvas.width/2, canvas.height/2);
+
+    player = new Player(canvas.width/2, canvas.height/2, 25, "black");
+
     for(i = 0; i < 100; i++) {
         let x = Math.random() * canvas.width;
         let y = Math.random() * canvas.height
@@ -32,13 +40,21 @@ function init() {
 
 function update() {
     c.clearRect(0, 0, canvas.width, canvas.height)
-    for(i = 0; i < 100; i++) {
+    for(i = 0; i < foods.length; i++) {
         foods[i].draw(c);
     }
+
+    player.draw(c);
 
     requestAnimationFrame(update);
 }
 
-window.addEventListener('load', function(event) {
+window.addEventListener('load', function() {
     init();
+
+    window.addEventListener('mousemove', function(event) {
+        mpos.x = event.clientX - canvas.offsetLeft;
+        mpos.y = event.clientY - canvas.offsetTop;
+        mpos.print();
+    });
 });

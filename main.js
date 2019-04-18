1f1f499fd12c020514c2cc91cc2c149303049c02
leokoernerCA <leo.koerner@concordacademy.org>
2019-04-18 10:14:30 -0400

    In class changes April 18th, 2019

--- main.js
+++ main.js
@@ -10,14 +10,29 @@ let player;
 let foods = [];
 let x;
 let y;
+let name = "";
 
-let colors = [
-    '#FFDC00',
-    '#FCB102',
-    '#00FF55',
-    '#E8FF54',
-    '#00FFA9'
-];
+let strokeColors = ['#006633', '#00B399', '#0033FF', '#00FF99', '#00B3E6',
+		  '#00B333', '#0066E6', '#009966', '#00FF99', '#004D4D',
+		  '#00B300', '#009900', '#00B3B3', '#0080B3', '#00991A',
+		  '#0099E6', '#00FF1A', '#001A66', '#00331A', '#00FFCC',
+		  '#00994D', '#0066CC', '#008000', '#003300', '#0080CC',
+		  '#00664D', '#001AFF', '#0066FF', '#00B3FF', '#00B399',
+		  '#0066B3', '#00991A', '#009999', '#00B31A', '#00E680',
+		  '#008066', '#009980', '#00FF80', '#00FF33', '#009933',
+		  '#003380', '#00CC00', '#00E64D', '#0080CC', '#0000B3',
+		  '#004D66', '#00B380', '#004D4D', '#00E6E6', '#0066FF']
+let colors = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6',
+		  '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
+		  '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A',
+		  '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
+		  '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC',
+		  '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
+		  '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680',
+		  '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
+		  '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3',
+		  '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'];
+
 
 function randomColor() {
     let index = Math.floor(Math.random() * colors.length);
@@ -34,7 +49,10 @@ function init() {
 
     mpos =  new Vector(canvas.width/2, canvas.height/2);
 
-    player = new Player(canvas.width/2, canvas.height/2, 25, "black");
+    let name = prompt("Enter Name");
+    let color = randomColor();
+    let stroke = strokeColors[colors.indexOf(color)];
+    player = new Player(canvas.width/2, canvas.height/2, 25, color, name, stroke);
 
     for(i = 0; i < FOOD_COUNT; i++) {
         generateFood();
--- player.js
+++ player.js
@@ -1,7 +1,25 @@
 class Player extends Food{
 
-    constructor(x, y, radius, color) {
+    constructor(x, y, radius, color, name, stroke) {
         super(x, y, radius, color);
+        this.name = name;
+        this.stroke = stroke;
+    }
+
+    draw(c) {
+        c.lineWidth = this.radius*.075;
+        c.strokeStyle = this.stroke;
+        super.draw(c);
+        c.stroke();
+        c.fillStyle = '#ffffff';
+        c.textAlign = 'center'
+        c.lineWidth = 5;
+        c.textBaseline = 'middle';
+        let fontSize = Math.round(this.radius*.33);
+        c.font = fontSize + 'px helvetica';
+        c.strokeStyle = '#000000';
+        c.strokeText(this.name, this.x, this.y);
+        c.fillText(this.name, this.x, this.y);
     }
 
 }
